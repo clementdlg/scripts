0e02cf458b61a7f07b7457fb139d66dd21493361
krem <0xkrem@proton.me>
2024-08-16 16:33:58 +0200

    WIP

--- lightdm_conf.sh
+++ lightdm_conf.sh
@@ -1,3 +1,36 @@
+
+# help
+if [[ ! -n "$1" ]]; then
+    echo "This script is part of KRem's Auto Bootstrap Script (KRABS)
+
+Its purpose is to configure display manager and sessions
+You must run it as root and pass a file path of the background image as an argument
+    Accepted extensions are .jpg and .png
+    Example : ./script '/path/to/image.png'"
+    exit 0
+fi
+
+# check if running as root
+if [[ $EUID -ne 0 ]]; then
+    echo "Error: Run this script as root"
+    exit 1
+fi
+
+# arg checking
+if [[ ! -f "$1" ]]; then
+    echo "Error : '$1' invalid image path'"
+    exit 1
+fi
+
+# extension checking
+ext=$(echo "$1" | sed 's/.*\.//')
+if [[ $ext != "png" && $ext != "jpg" ]] ;then
+    echo "Error : .$ext file is invalid"
+    exit 1
+fi
+
+img="$1"
+
 # check lightdm is installed
 if ! which lightdm ; then
     echo "Installing lightdm..."
@@ -8,17 +41,19 @@ if ! which lightdm-gtk-greeter ; then
     echo "Installing lightdm-gtk-greeter..."
     dnf install lightdm-gtk-greeter -y >/dev/null
 fi
+
 # configure services
 echo "Configuring service and target"
 systemctl enable lightdm.service >/dev/null
 systemctl set-default graphical.target >/dev/null
 
 # background image
-
-cp /home/krem/.config/awesome/theme/lockscreen-bg-fhd.png /usr/share/backgrounds/
-chmod 644 /usr/share/backgrounds/lockscreen-bg-fhd.png
+cp "$img" /usr/share/backgrounds/
+chmod 644 "/usr/share/backgrounds/$img"
 
 # config files
+echo "setting up .conf files"
+
 echo "[Seat:*]
 autologin-user=krem
 autologin-session=xfce
