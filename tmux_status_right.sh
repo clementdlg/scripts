e4bc94df52d8de5c567638a49449e15d7e118a64
Clément DE LA GENIERE <0xkrem@proton.me>
2024-12-23 05:19:17 +0100

    tmux status right

--- tmux_status_right.sh
+++ tmux_status_right.sh
@@ -1,16 +1,31 @@
 #!/bin/env bash
 
 if [[ -z "$1" ]]; then
-	return
-fi
-
-git -C "$1" rev-parse --is-inside-work-tree &> /dev/null
-if [[ $? -ne 0 ]]; then
+	# current directory must be passed
 	exit 0
 fi
 
-branch=$(git -C "$1"  rev-parse --abbrev-ref HEAD)
+# format
+fseparator="#[fg=#94b6ff]#[bg=#1c1c2e]"
+tseparator="#[fg=#94b6ff]#[bg=#1c1c2e]"
 
-separator="#[fg=#94b6ff]#[bg=#1c1c2e]"
-text="#[bg=#94b6ff]#[fg=#1c1c2e] $branch"
-echo "$separator$text"
+# git branch
+git -C "$1" rev-parse --is-inside-work-tree &> /dev/null
+if [[ $? -eq 0 ]]; then
+	branch=$(git -C "$1"  rev-parse --abbrev-ref HEAD)
+	git="#[bg=#94b6ff]#[fg=#1c1c2e] $branch"
+	git="$fseparator$git$tseparator"
+fi
+
+# ip
+ip=$(~/scripts/get_private_ip.sh)
+if [[ "$ip" != "disconnected" ]]; then
+	ip_symbol="󰈀"
+else
+	ip_symbol=""
+fi
+
+ip="#[bg=#94b6ff]#[fg=#1c1c2e]$ip_symbol $ip"
+
+# output
+echo "$git$fseparator$ip$tseparator"
