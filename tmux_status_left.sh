b7a3081948a734f076c3b4f9f4039fcb8113da01
Clément DE LA GENIERE <0xkrem@proton.me>
2024-12-26 22:16:59 +0100

    update tmux script

--- tmux_status_left.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-# check if session exist
-if [[ -z "$1"]]; then
-	exit
-fi
-echo $1
-
-# tmux list-windows -F "#I:#W"
--- tmux_status_right.sh
+++ tmux_status_right.sh
@@ -1,31 +1,28 @@
 #!/bin/env bash
 
-if [[ -z "$1" ]]; then
-	# current directory must be passed
-	exit 0
-fi
-
 # format
-fseparator="#[fg=#94b6ff]#[bg=#1c1c2e]"
-tseparator="#[fg=#94b6ff]#[bg=#1c1c2e]"
+colo="#[bg=#94b6ff]#[fg=#1c1c2e]"
+s=""
+e=""
 
 # git branch
 git -C "$1" rev-parse --is-inside-work-tree &> /dev/null
 if [[ $? -eq 0 ]]; then
 	branch=$(git -C "$1"  rev-parse --abbrev-ref HEAD)
-	git="#[bg=#94b6ff]#[fg=#1c1c2e] $branch"
-	git="$fseparator$git$tseparator"
+	git=" $branch"
+	git="$s$git$e"
 fi
 
-# ip
-ip=$(~/scripts/get_private_ip.sh)
-if [[ "$ip" != "disconnected" ]]; then
-	ip_symbol="󰈀"
-else
-	ip_symbol=""
-fi
+# directory
+sess="$2"
+sess=" $sess"
+sess="$s$sess$e"
 
-ip="#[bg=#94b6ff]#[fg=#1c1c2e]$ip_symbol $ip"
+# wireguard
+if [[ -n $(wg show 2>&1) ]]; then
+	wg="󰞉 wireguard"
+	wg="$s$wg$e"
+fi
 
 # output
-echo "$git$fseparator$ip$tseparator"
+echo "$colo$wg$distro$git$sess"
