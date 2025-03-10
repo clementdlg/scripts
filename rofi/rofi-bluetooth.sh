564c03702c74e11543fb77c95d15ae5146a606ee
Clément DE LA GENIERE <0xkrem@proton.me>
2025-03-10 20:12:17 +0100

    rofi-bt: heavy refactor

--- rofi/rofi-bluetooth.sh
+++ rofi/rofi-bluetooth.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-SCAN_TIMEOUT=6
+SCAN_TIMEOUT=4
 
 is_installed() {
 	[[ -z "$1" ]] && return 1
@@ -25,54 +25,64 @@ silent() {
 	"$@" &>/dev/null
 }
 
-paired() {
-	local devices="$(bluetoothctl devices Paired | awk '{$1=$2=""; print substr($0,3)}')"
+choose_device() {
+	local type="$1"
+	if ! echo "$type" | grep -E "Connected|Paired"; then
+		type=""
+	fi
+	
+	local message="$2"
+	[[ -z "$message" ]] && return 1
 
-	printf "%s\n" "$devices" | rofi show -dmenu -p "Connect a device"
+	local devices="$(bluetoothctl devices $type | awk '{$1=$2=""; print substr($0,3)}')"
+
+	# printf "%s\n" "$devices" # debug
+
+	device="$(printf "%s\n" "$devices" | rofi show -dmenu -p "$message")"
+
+	echo "$device"
 }
 
-connected() {
-	# show list
-	local devices="$(bluetoothctl devices Connected | awk '{$1=$2=""; print substr($0,3)}')"
-	printf "%s\n" "$devices" | rofi show -dmenu -p "Disconnect a device"
+get_mac_addr() {
+	device="$1"
+	[[ -z "$device" ]] && return 1
 
-	notify "Disconnecting $devices"
+	bluetoothctl devices | grep "$device" | cut -f2 -d' '
+}
 
-	# disconnection
-	local mac=$(bluetoothctl devices | grep "$device" | cut -f2 -d' ')
+disconnect_device_menu() {
+	device="$(choose_device "Connected" "Disconnect a device")"
+	mac="$(get_mac_addr "$device")"
+
+	notify "Disconnecting $device"
 	silent bluetoothctl disconnect "$mac"
 }
 
-rm_devices() {
-	local devices="$(bluetoothctl devices | awk '{$1=$2=""; print substr($0,3)}')"
-
-	local device=$(printf "%s\n" "$devices" | rofi show -dmenu -p "Remove a device")
-
-	local mac=$(bluetoothctl devices | grep "$device" | cut -f2 -d' ')
+rm_devices_menu() {
+	device="$(choose_device "Paired" "Remove a device")"
+	mac="$(get_mac_addr "$device")"
 
 	notify "Removing $device"
-
 	silent bluetoothctl remove "$mac"
 }
 
-scan() {
+get_scanned_device() {
 	notify "Scanning for devices..."
 
 	silent bluetoothctl -t $SCAN_TIMEOUT scan on
 
-	local devices="$(bluetoothctl devices | awk '{$1=$2=""; print substr($0,3)}')"
+	device="$(choose_device "" "Connect a device")"
 
-	printf "%s\n" "$devices" | rofi show -dmenu -p "Connect a device"
+	echo "$device"
 }
 
 connect_device() {
 	local device="$1"
 	[[ -z "$device" ]] && return 1
 
-	local mac=$(bluetoothctl devices | grep "$device" | cut -f2 -d' ')
+	mac="$(get_mac_addr "$device")"
 
 	notify "Attempting to connect to $device"
-
 	silent bluetoothctl connect "$mac"
 }
 
@@ -101,18 +111,18 @@ menu_enabled() {
 
 	case "$choice" in
 		"$connect_menu")
-			connected
+			disconnect_device_menu
 			;;
 		"$paired_menu")
-			local device=$(paired)
+			device="$(choose_device "Paired" "Connect a device")"
 			connect_device "$device"
 			;;
 		"$scan_menu")
-			local device=$(scan)
+			device="$(get_scanned_device)"
 			connect_device "$device"
 			;;
 		"$remove_menu")
-			rm_devices
+			rm_devices_menu
 			;;
 		"$disable")
 			silent bluetoothctl power off
